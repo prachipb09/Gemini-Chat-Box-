8e90a96d4d0ba38626616e7f04c5325f31d78bdf
prachipb09 <prachipb09@gmail.com>
2024-07-25 20:03:42 +0530

    Initial Commit

---
+++ Gemini Chat Box/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Gemini Chat Box
+//
+//  Created by Prachi Bharadwaj on 25/07/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Gemini Chat Box/Gemini_Chat_BoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gemini_Chat_BoxApp.swift
+//  Gemini Chat Box
+//
+//  Created by Prachi Bharadwaj on 25/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Gemini_Chat_BoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
