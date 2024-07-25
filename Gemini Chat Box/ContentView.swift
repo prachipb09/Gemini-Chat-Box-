2aadeb99d9f6997a82ffdb89d45a22f74a770c79
prachipb09 <prachipb09@gmail.com>
2024-07-25 21:05:17 +0530

    no message

--- Gemini Chat Box/ContentView.swift
+++ Gemini Chat Box/ContentView.swift
@@ -8,14 +8,72 @@
 import SwiftUI
 
 struct ContentView: View {
+    private let viewModel = ChatBotViewModel()
+    @State private var textField = ""
+    @State private var showLoader: Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            ScrollView {
+                VStack {
+                    if let response = viewModel.response {
+                        Text(response)
+                    }
+                }
+            }
+                Spacer()
+                
+                HStack {
+                    TextField(text: $textField) {
+                        Text("Ask me....")
+                    }.onSubmit {
+                        sendQuestion(textField)
+                    }
+                    
+                    Button(action: {
+                        sendQuestion(textField)
+                        
+                    }, label: {
+                        Image(systemName: "arrow.up.circle")
+                            .foregroundColor(.blue)
+                    })
+                }
+            
         }
-        .padding()
+            .padding()
+            .showLoader(showLoader: showLoader)
+        
+    }
+    
+    func sendQuestion(_ question: String) {
+        showLoader = true
+        textField = ""
+        if !question.isEmpty {
+            Task {
+                try await viewModel.generateAnswer(question: question)
+                showLoader = false
+                
+            }
+        }
+        
+    }
+}
+
+struct ProgressViewScreen: ViewModifier {
+    var showLoader: Bool
+    func body(content: Content) -> some View {
+        content
+            .overlay {
+            if showLoader {
+                ProgressView()
+                    .progressViewStyle(.circular)
+            }
+        }
+    }
+}
+
+extension View {
+    func showLoader(showLoader: Bool) -> some View {
+        return modifier(ProgressViewScreen(showLoader: showLoader))
     }
 }
 
